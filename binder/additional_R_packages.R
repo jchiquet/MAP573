1dfafd85faf2320af3218fe2c5eb5b718ec94152
Julien Chiquet <julien.chiquet@gmail.com>
2020-09-16 15:15:53 +0200

    updating binder do_binder do_website

--- binder/additional_R_packages.R
+++ binder/additional_R_packages.R
@@ -6,3 +6,7 @@ local({
 
 ## non CRAN packages
 
+library(knitr)       # R notebook export and formatting 
+install.packages("factoextra")  # Fancy plotting of FactoMineR outputs
+install.packages("FactoMineR")  # Factor analysis
+install.packages("GGally") # paris plots
