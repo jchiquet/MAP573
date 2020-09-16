4579a68b527c0c108f4c9c961ed7a02ff6fa141d
jchiquet <jchiquet@users.noreply.github.com>
2020-09-16 13:43:07 +0000

    deploy: 449abd970a6b82b4a65904f20eb9d56088944ccc

--- binder/additional_R_packages.R
+++ binder/additional_R_packages.R
@@ -6,3 +6,7 @@ local({
 
 ## non CRAN packages
 
+install.packages("factoextra")  # Fancy plotting of factor-based methods
+install.packages("FactoMineR")  # Factor analysis
+install.packages("GGally")      # pairs plots
+install.packages("kableExtra")  # fancy kable
